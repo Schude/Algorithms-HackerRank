a2202c2bdb1d62be2cc44e7abc84094d310e1784
Schude <nemli.serkan2@gmail.com>
2021-05-15 21:45:29 +0300

    Cats and Mouse

---
+++ Implementation/cats-and-mouse/index.js
@@ -0,0 +1,9 @@
+function catAndMouse(x, y, z) {
+    if (Math.abs(z - y) < Math.abs(z - x)) {
+        return "Cat B";
+    } else if (Math.abs(z - y) > Math.abs(z - x)) {
+        return "Cat A";
+    } else {
+        return "Mouse C";
+    }
+}
